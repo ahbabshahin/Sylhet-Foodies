63b40e5dbf775e51546bbc8e57a474177853f02f
Ahbab <ahbabshahin7788@gmail.com>
2023-09-29 19:16:16 +0600

    app js, and dependancy files for auth been added

--- server-side/app.js
+++ server-side/app.js
@@ -2,32 +2,38 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+const connectDB = require('./db/connect');
 
-// error handler
+const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./routes/authRoutes');
+const blogRoutes = require('./routes/blogRoutes');
+const menuRoutes = require('./routes/menuRoutes');
+const restaurantRoutes = require('./routes/inventoryRoutes');
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
-// extra packages
 
-// routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/blogs', blogRoutes);
+app.use('/api/v1/menus', menuRoutes);
+app.use('/api/v1/restaurants', restaurantRoutes);
 
+// Error handling middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
-  try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen(port, console.log(`Server is listening on port: ${port}`));
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 start();
